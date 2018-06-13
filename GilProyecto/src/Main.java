5b6f4c18c257dd2c2cf39c31556f75ac26e0f09f
alwaysmile24 <vanne24_smile@hotmail.com>
2018-06-12 21:50:23 -0500

    Update Main.java

--- GilProyecto/src/Main.java
+++ GilProyecto/src/Main.java
@@ -3,6 +3,20 @@ import csv.*;
 
 public class Main{
 	public static void main(String[] args) {
+		Scanner lector = new Scanner(System.in);
+		int opcion;
+		do {
+			System.out.println("Qué desea hacer?");
+			System.out.println("1. Agregar un nuevo cliente");
+			System.out.println("2. Consultar un cliente");
+			System.out.println("3. Eliminar un cliente");
+			System.out.println("4. Salir");
+			opcion = lector.nextInt();
+			switch(opcion){
+				case 1:
+					
+			
+
 		ClavesEntidades entidades = new ClavesEntidades();
 		
 	}
